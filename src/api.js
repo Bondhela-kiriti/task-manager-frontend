a19f60c9c9476f5a67e09073d16462cf9c0dba3c
KIRITI <162373170+Bondhela-kiriti@users.noreply.github.com>
2025-09-07 08:49:56 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://52f83936-854e-486d-baff-5a833e82fd9d-00-1feeoixxxan1w.pike.replit.dev/api/", // change to Render URL after deploy
+  baseURL: "https://taskmanager-backend-gv7s.onrender.com", // change to Render URL after deploy
 });
 
 // Add token automatically if exists
